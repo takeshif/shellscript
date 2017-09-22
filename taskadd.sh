f7414cb5af0b334dcfbda649c3f6303bc811bb65
Tfujimoto <jinko.tatu@gmail.com>
2017-09-22 15:25:51 +0900

    taskadd.shをaccept入力した and gitignore設定

--- taskadd.sh
+++ taskadd.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-echo "プロジェクト名を入力："
-read project
+echo "タスクのタイプ(today or other)："
+read tasktype
+if [ $tasktype = today ] 
+then
+  project="TODO"
+else
+  echo "プロジェクト名を入力："
+  read project
+fi
 echo "プライオリティを入力(H or M or L)："
 read pri
-echo "締めを入力："
-read due
+if [ $tasktype = today ]
+then
+  due="today"
+else
+  echo "締めを入力："
+  read due
+fi
 echo "内容を入力："
 read content
 task add project:$project pri:$pri due:$due $content
